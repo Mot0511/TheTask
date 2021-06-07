7296ec27618fde2245501b72a1c19ebb5eed0b80
Mot0511 <suvorov.matvej9@gmail.com>
2021-06-07 19:50:49 +0300

    _

--- web/script.js
+++ web/script.js
@@ -34,17 +34,17 @@ isplan++
 }
 
 isrename = true
-function rename() {
+function rename(oldname, idname) {
   if (isrename){
-    document.getElementById('tab1').innerHTML = '<input type="text" id="newname" class="new_name_input" placeholer="введите новое имя">'
+    document.getElementById('tab' + idname).innerHTML = '<input type="text" id="newname" class="new_name_input">'
     document.getElementById('rename_bt').innerHTML = '<img src="img/save.png">'
     isrename = false
   }
   else{
     newname = document.getElementById('newname').value + '.txt'
     document.getElementById('rename_bt').innerHTML = '<img src="img/pencil.png">'
-    document.getElementById('tab1').innerHTML = newname
-    eel.savename(newname, 'plan1.txt');
+    document.getElementById('tab' + idname).innerHTML = newname
+    eel.savename(newname, oldname + '.txt');
     isrename = true
   }
 }
